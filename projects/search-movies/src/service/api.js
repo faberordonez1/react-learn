f65c9f990a8b507f20972ff2f87a5a6e52397101
Faber OrdoñeZ <faberordonez1@gmail.com>
2025-05-29 12:33:08 -0500

    eslint

--- projects/search-movies/src/service/api.js
+++ projects/search-movies/src/service/api.js
@@ -1,29 +1,28 @@
 // let API = 'https://api.themoviedb.org/3/movie/550';
-let API='https://api.themoviedb.org/3/search/collection';
-let api_key ='e1ee3a585e3ff2efd9d0a990db15e813';
+const API = 'https://api.themoviedb.org/3/search/collection'
+const apiKey = 'e1ee3a585e3ff2efd9d0a990db15e813'
 
-
-export async function getMovies (query)  {
-  return fetch(API + '?api_key=' + api_key+ '&query='+query)
-  .then((resp)=> resp.json())
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+export async function getMovies (query) {
+  return fetch(API + '?api_key=' + apiKey + '&query=' + query)
+    .then((resp) => resp.json())
+    .catch((error) => {
+      console.error('Error:', error)
+    })
 }
 
-export async function getUrlImg ()  {
-  let URL_IMG='';
+export async function getUrlImg () {
+  let URL_IMG = ''
 
-  if (URL_IMG) return URL_IMG;
+  if (URL_IMG) return URL_IMG
 
-  let API_CONFIG = 'https://api.themoviedb.org/3/configuration?api_key='+api_key;
+  const API_CONFIG = 'https://api.themoviedb.org/3/configuration?api_key=' + apiKey
   return fetch(API_CONFIG)
-  .then((resp)=> resp.json())
-  .then(({images}) => {
-    let baseUrl = images.secure_base_url;
-    let size =images.still_sizes[1];
-    URL_IMG = baseUrl + size;
-    return URL_IMG;
-  })
-  .catch(() =>  '');
+    .then((resp) => resp.json())
+    .then(({ images }) => {
+      const baseUrl = images.secure_base_url
+      const size = images.still_sizes[1]
+      URL_IMG = baseUrl + size
+      return URL_IMG
+    })
+    .catch(() => '')
 }
